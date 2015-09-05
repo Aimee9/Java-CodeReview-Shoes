332d35a7f7389459779cc338be803b6c72602007
Aimee <akmreiss@yahoo.com>
2015-09-05 16:38:40 -0700

    Update database

--- shoes_test.sql
+++ shoes_test.sql
@@ -30,7 +30,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: brands; Type: TABLE; Schema: public; Owner: Guest; Tablespace: 
+-- Name: brands; Type: TABLE; Schema: public; Owner: oem; Tablespace: 
 --
 
 CREATE TABLE brands (
@@ -39,10 +39,10 @@ CREATE TABLE brands (
 );
 
 
-ALTER TABLE brands OWNER TO "Guest";
+ALTER TABLE brands OWNER TO oem;
 
 --
--- Name: brand_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+-- Name: brand_id_seq; Type: SEQUENCE; Schema: public; Owner: oem
 --
 
 CREATE SEQUENCE brand_id_seq
@@ -53,17 +53,17 @@ CREATE SEQUENCE brand_id_seq
     CACHE 1;
 
 
-ALTER TABLE brand_id_seq OWNER TO "Guest";
+ALTER TABLE brand_id_seq OWNER TO oem;
 
 --
--- Name: brand_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+-- Name: brand_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: oem
 --
 
 ALTER SEQUENCE brand_id_seq OWNED BY brands.id;
 
 
 --
--- Name: brands_stores; Type: TABLE; Schema: public; Owner: Guest; Tablespace: 
+-- Name: brands_stores; Type: TABLE; Schema: public; Owner: oem; Tablespace: 
 --
 
 CREATE TABLE brands_stores (
@@ -73,10 +73,10 @@ CREATE TABLE brands_stores (
 );
 
 
-ALTER TABLE brands_stores OWNER TO "Guest";
+ALTER TABLE brands_stores OWNER TO oem;
 
 --
--- Name: brands_stores_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+-- Name: brands_stores_id_seq; Type: SEQUENCE; Schema: public; Owner: oem
 --
 
 CREATE SEQUENCE brands_stores_id_seq
@@ -87,17 +87,17 @@ CREATE SEQUENCE brands_stores_id_seq
     CACHE 1;
 
 
-ALTER TABLE brands_stores_id_seq OWNER TO "Guest";
+ALTER TABLE brands_stores_id_seq OWNER TO oem;
 
 --
--- Name: brands_stores_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+-- Name: brands_stores_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: oem
 --
 
 ALTER SEQUENCE brands_stores_id_seq OWNED BY brands_stores.id;
 
 
 --
--- Name: stores; Type: TABLE; Schema: public; Owner: Guest; Tablespace: 
+-- Name: stores; Type: TABLE; Schema: public; Owner: oem; Tablespace: 
 --
 
 CREATE TABLE stores (
@@ -106,10 +106,10 @@ CREATE TABLE stores (
 );
 
 
-ALTER TABLE stores OWNER TO "Guest";
+ALTER TABLE stores OWNER TO oem;
 
 --
--- Name: store_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+-- Name: store_id_seq; Type: SEQUENCE; Schema: public; Owner: oem
 --
 
 CREATE SEQUENCE store_id_seq
@@ -120,83 +120,93 @@ CREATE SEQUENCE store_id_seq
     CACHE 1;
 
 
-ALTER TABLE store_id_seq OWNER TO "Guest";
+ALTER TABLE store_id_seq OWNER TO oem;
 
 --
--- Name: store_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+-- Name: store_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: oem
 --
 
 ALTER SEQUENCE store_id_seq OWNED BY stores.id;
 
 
 --
--- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+-- Name: id; Type: DEFAULT; Schema: public; Owner: oem
 --
 
 ALTER TABLE ONLY brands ALTER COLUMN id SET DEFAULT nextval('brand_id_seq'::regclass);
 
 
 --
--- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+-- Name: id; Type: DEFAULT; Schema: public; Owner: oem
 --
 
 ALTER TABLE ONLY brands_stores ALTER COLUMN id SET DEFAULT nextval('brands_stores_id_seq'::regclass);
 
 
 --
--- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+-- Name: id; Type: DEFAULT; Schema: public; Owner: oem
 --
 
 ALTER TABLE ONLY stores ALTER COLUMN id SET DEFAULT nextval('store_id_seq'::regclass);
 
 
 --
--- Name: brand_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+-- Name: brand_id_seq; Type: SEQUENCE SET; Schema: public; Owner: oem
 --
 
-SELECT pg_catalog.setval('brand_id_seq', 18, true);
+SELECT pg_catalog.setval('brand_id_seq', 11, true);
 
 
 --
--- Data for Name: brands; Type: TABLE DATA; Schema: public; Owner: Guest
+-- Data for Name: brands; Type: TABLE DATA; Schema: public; Owner: oem
 --
 
 COPY brands (id, label) FROM stdin;
+9	Nike
+10	Converse
+11	Adidas
 \.
 
 
 --
--- Data for Name: brands_stores; Type: TABLE DATA; Schema: public; Owner: Guest
+-- Data for Name: brands_stores; Type: TABLE DATA; Schema: public; Owner: oem
 --
 
 COPY brands_stores (id, brand_id, store_id) FROM stdin;
+29	9	8
+31	9	11
+32	9	10
 \.
 
 
 --
--- Name: brands_stores_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+-- Name: brands_stores_id_seq; Type: SEQUENCE SET; Schema: public; Owner: oem
 --
 
-SELECT pg_catalog.setval('brands_stores_id_seq', 1, false);
-
-
---
--- Name: store_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
---
-
-SELECT pg_catalog.setval('store_id_seq', 41, true);
+SELECT pg_catalog.setval('brands_stores_id_seq', 32, true);
 
 
 --
--- Data for Name: stores; Type: TABLE DATA; Schema: public; Owner: Guest
+-- Name: store_id_seq; Type: SEQUENCE SET; Schema: public; Owner: oem
+--
+
+SELECT pg_catalog.setval('store_id_seq', 11, true);
+
+
+--
+-- Data for Name: stores; Type: TABLE DATA; Schema: public; Owner: oem
 --
 
 COPY stores (id, name) FROM stdin;
+8	Shoepie
+9	Forrest's Big Foot
+10	Monkey Feet
+11	Drumline
 \.
 
 
 --
--- Name: brand_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace: 
+-- Name: brand_pkey; Type: CONSTRAINT; Schema: public; Owner: oem; Tablespace: 
 --
 
 ALTER TABLE ONLY brands
@@ -204,7 +214,7 @@ ALTER TABLE ONLY brands
 
 
 --
--- Name: brands_stores_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace: 
+-- Name: brands_stores_pkey; Type: CONSTRAINT; Schema: public; Owner: oem; Tablespace: 
 --
 
 ALTER TABLE ONLY brands_stores
@@ -212,7 +222,7 @@ ALTER TABLE ONLY brands_stores
 
 
 --
--- Name: store_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace: 
+-- Name: store_pkey; Type: CONSTRAINT; Schema: public; Owner: oem; Tablespace: 
 --
 
 ALTER TABLE ONLY stores
@@ -220,12 +230,12 @@ ALTER TABLE ONLY stores
 
 
 --
--- Name: public; Type: ACL; Schema: -; Owner: epicodus
+-- Name: public; Type: ACL; Schema: -; Owner: oem
 --
 
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM epicodus;
-GRANT ALL ON SCHEMA public TO epicodus;
+REVOKE ALL ON SCHEMA public FROM oem;
+GRANT ALL ON SCHEMA public TO oem;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 

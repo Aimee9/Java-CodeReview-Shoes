1a299180b0473ebbabbe50df8378bcdf90f96dde
Aimee <akmreiss@yahoo.com>
2015-09-05 17:13:54 -0700

    Add more tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -48,12 +48,41 @@ public class AppTest extends FluentTest {
     assertThat(pageSource()).contains("Shoepie");
     }
 
+  @Test
+  public void updateStoreNameWorks() {
+    Store myStore = new Store("Shoepie");
+    myStore.save();
+    String storePath = String.format("http://localhost:4567/store/%d", myStore.getId());
+    goTo(storePath);
+    fill("#updateName").with("Monkey Feet");
+    submit("btn btn-warning");
+    assertThat(pageSource()).contains("Monkey Feet");
+    }
+
     // @Test
     // public void deleteIndividualStore() {
     //   Store myStore = new Store("Shoepie");
     //   myStore.save();
     //   String storePath = String.format("http://localhost:4567/store/%d", myStore.getId());
     //   goTo(storePath);
-    //   assertThat(pageSource()).contains("Shoepie");
+    //   click("/delete/store/$store.getId()");
+    //   goTo("http://localhost:4567/stores");
+    //   assertThat(pageSource()).doesNotContain("Shoepie");
     //   }
+
+    @Test
+    public void addsBrands(){
+      goTo("http://localhost:4567/brands");
+      fill("#newBrand").with("Adidas");
+      submit(".btn btn-info");
+      assertThat(pageSource()).contains("Adidas");
+    }
+    @Test
+    public void individualBrandLinksWork() {
+      Brand myBrand = new Brand("Adidas");
+      myBrand.save();
+      String brandPath = String.format("http://localhost:4567/brand/%d", myBrand.getId());
+      goTo(brandPath);
+      assertThat(pageSource()).contains("Adidas");
+      }
 }
